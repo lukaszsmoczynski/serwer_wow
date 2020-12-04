972a12ed00792f14cd200a406d33851023851075
lukaszsmoczynski <34213371+lukaszsmoczynski@users.noreply.github.com>
2020-12-04 13:59:40 +0100

    Update serwer_wow.bat

--- serwer_wow.bat
+++ serwer_wow.bat
@@ -1,15 +1,20 @@
 echo off
 cls
-echo Na ktory serwer chcesz sie zalogowac?
-echo [1]: Sunwell
-echo [2]: Warmane
-set /p serwer=
 
 IF EXIST %cd%\Data\enUS (
   set folder=%cd%\Data\enUS\
 ) ELSE IF EXIST %cd%\Data\enGB (
   set folder=%cd%\Data\enGB\
+) ELSE (
+  echo Nie udalo sie ustalic lokalizacji pliku realmlist
+  GOTO END
 )
+
+:SELECT_SERVER
+echo Na ktory serwer chcesz sie zalogowac?
+echo [1]: Sunwell
+echo [2]: Warmane
+set /p serwer=
 
 echo %folder%
 pause
@@ -18,6 +23,12 @@ IF "%serwer%"=="1" (
   echo set realmlist logon.sunwell.pl> %folder%\realmlist.wtf
 ) ELSE IF "%serwer%"=="2" (
   echo set realmlist logon.warmane.com> %folder%\Data\enUS\realmlist.wtf
+) ELSE (
+  cls
+  echo Niepoprawna odpowiedz
+  GOTO SELECT_SERVER
 )
 
 start %cd%\Wow.exe
+
+:END
